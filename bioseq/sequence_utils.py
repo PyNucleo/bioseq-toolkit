e83a155a34d99724496ca1899a0a130ff4dbd1ed
PyNucleo <adamaboulhosn2@gmail.com>
2026-05-21 14:50:55 +0300

    Refactored utilities and fixed Smith-Waterman traceback logic

--- bioseq/sequence_utils.py
+++ bioseq/sequence_utils.py
@@ -1,43 +1,41 @@
 from collections import Counter
 #Dicts for transcribing template & coding strands
-DICT_CONVERTTemplate = {"A":"U",
+DNA_TEMPLATE_TO_MRNA = {"A":"U",
               "G":"C",
               "T":"A",
               "C":"G"}
-DICT_CONVERTCoding = {"A":"A",
+DNA_CODING_TO_MRNA = {"A":"A",
               "G":"G",
               "T":"U",
               "C":"C"}
 
-def total_length(DNA):
-    return len(DNA)
+DNA_COMPLEMENT = {
+    "A": "T",
+    "T": "A",
+    "G": "C",
+    "C": "G"
+}
 
-def base_number(DNA): 
-    return Counter(DNA)
+def total_length(sequence):
+    return len(sequence)
 
-def gc_content(DNA):
-    S = base_number(DNA)
-    C_Content = S["C"]
-    G_Content = S["G"]
-    GC_Hundo = (C_Content + G_Content) / total_length(DNA)
-    return f"{GC_Hundo * 100:.2f}"
+def base_number(sequence): 
+    return Counter(sequence)
 
-def reverse_complement(DNA):
-    DNA = DNA[::-1]
-    E = ''
-    for comp in range(0, len(DNA)):
-        if DNA[comp]=="A":
-            E+="T"
-        elif DNA[comp]=="T":
-            E+="A"
-        elif DNA[comp]=="G":
-            E+="C"
-        else:
-            E+="G"
-    return E
+def gc_content(sequence):
+    if not sequence:
+        return 0.0
+    
+    counts = base_number(sequence)
+
+    gc_fraction = (counts["G"] + counts["C"]) / total_length(sequence)
+    return gc_fraction * 100
+
+def reverse_complement(dna_sequence):
+    return "".join(DNA_COMPLEMENT[base] for base in dna_sequence)
 
 #Transcription:
-def mrna_template(DNA_Sequence):
-    return ''.join(DICT_CONVERTTemplate[base] for base in DNA_Sequence)
-def mrna_coding(DNA_Sequence):
-    return ''.join(DICT_CONVERTCoding[base] for base in DNA_Sequence)
+def mrna_template(dna_sequence):
+    return "".join(DNA_TEMPLATE_TO_MRNA[base] for base in dna_sequence)
+def mrna_coding(dna_sequence):
+    return ''.join(DNA_CODING_TO_MRNA[base] for base in dna_sequence)
